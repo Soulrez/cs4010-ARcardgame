f7bc644b66aab4c3c68f47c4c1ed9ef0ce8d6768
Richard Yang <ruiqi888@gmail.com>
2017-05-10 13:12:35 -0600

    Marker template

---
+++ Assets/Model_Change.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Model_Change : MonoBehaviour {
+
+	public GameObject modelA;
+    public GameObject modelB;
+    public GameObject modelC;
+    public GameObject modelD;
+    public string key;
+    private int modelNumber;
+
+	// Use this for initialization
+	void Start () {
+		 modelNumber = 1;
+		 modelB.transform.localScale = new Vector3(0, 0, 0);
+		 modelC.transform.localScale = new Vector3(0, 0, 0);
+		 modelD.transform.localScale = new Vector3(0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(key)){
+       		ModelSwitch();
+		}
+	}
+
+  	void ModelSwitch(){
+		if(modelNumber == 1){
+			modelA.transform.localScale = new Vector3(0, 0, 0);
+			modelB.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+			modelNumber = 2;            
+		}else if(modelNumber == 2){
+			modelB.transform.localScale = new Vector3(0, 0, 0);
+			modelC.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+			modelNumber = 3;
+		}else if(modelNumber == 3){
+			modelC.transform.localScale = new Vector3(0, 0, 0);
+			modelD.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+			modelNumber = 4;
+		}else if(modelNumber == 4){
+			modelD.transform.localScale = new Vector3(0, 0, 0);
+			modelA.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+			modelNumber = 1;
+		}
+	}
+}
---
+++ README.txt
@@ -0,0 +1,10 @@
+COSC 4010: Virtual Reality Systems
+Project 3
+Richard Yang
+
+The custom patterns are available as PDFs in the root directory.
+
+Press 's' to change the model on the Hiro Marker
+Press 'a' to change the model on the Kanji Marker
+Press 'w' to change the model on the Elf Marker
+Press 'd' to change the model on the Knight Marker
